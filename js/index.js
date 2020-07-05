c4553a7015f5a8449aca7848405673c812f31ec4
Billonyohan <yohan.billon@yahoo.fr>
2020-07-05 19:00:16 +0200

    changement organisation fichier // changement page principale

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Cree la fenetre du navigateur.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('html/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+const {Pool, Client} = require('pg')
+client = new Client({
+    host: 'localhost',
+    user: 'yohan',
+    password: 'logitech',
+    database: 'veleos',
+});
+
